6f185c2343b0aabcef033088ed2f30db968b4f1a
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-13 09:43:52 +0200

    ♻️ countries service and loading spinner

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,37 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../interfaces/country';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
-  private apiUrl: string = 'https://restcountries.com/v3.1';
   constructor(private http: HttpClient) {}
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
 
   searchByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${code}`;
 
     return this.http.get<Country[]>(url).pipe(
+      // map((countries) => (countries.length > 0 ? countries[0] : null)),
       map((countries) => countries[0]),
       catchError(() => of(null))
     );
   }
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(
+      catchError(() => of([])),
+      delay(2000)
+    );
+  }
+
   searchByCapital(query: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${query}`;
-
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchByName(query: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${query}`;
-
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchByRegion(query: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${query}`;
-
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     HomePageComponent,
     SidebarComponent,
     SearchBoxComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [CommonModule, RouterModule],
   exports: [
@@ -23,6 +25,7 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     HomePageComponent,
     SidebarComponent,
     SearchBoxComponent,
+    LoadingSpinnerComponent,
   ],
 })
 export class SharedModule {}
